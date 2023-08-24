dfe6716680be0ca7354b8bf5cb0a8cb496363f78
william <floydw1234>
2023-08-24 13:34:23 -0600

    updated port

--- app.py
+++ app.py
@@ -116,5 +116,5 @@ if __name__ == "__main__":
     if args.get("--prod", 'n').lower() == 'y':
         app.run(host="0.0.0.0",port=80, debug=False)
     else:
-        app.run(host="0.0.0.0",port=5000, debug=True, ssl_context=("cert.pem", "key.pem"))
+        app.run(host="0.0.0.0",port=10000, debug=True, ssl_context=("cert.pem", "key.pem"))
 
