c24f42becf12cc1a11d5ecd0529ea87f68857b9f
floydw1234 <floydw1234@gmail.com>
2024-05-23 22:32:37 -0600

    working branches and nodes with correct spacing, pushes things to the right when there is not enough space

--- static/js/draw.js
+++ static/js/draw.js
@@ -20,24 +20,83 @@ let offsetY = 20;
 var blackColor ="rgb(0 0 0 / 75%)"
 var greyColor = "rgb(0 0 0 / 25%)"
 
-let starting_x = 600
+let starting_x = 750
 let starting_y = 2
 
-var nodes = []
+let rectWidth = 150
+let rectHeight = 100
+let line_color = "rgb(0 0 0 / 50%)"
+let background_color = "white"
 
-// var nodes = [{
-//     nodeId:"1",
-//     x: 600,
-//     y: 2,
-//     height:100,
-//     width:150,
-//     line_color:"rgb(0 0 0 / 50%)",
-//     background_color:"white",
-//     text:"",
-//     type:"",
-//     icon:"",
-//     click:"",
-// }]
+let optionRectWidth = 100
+let optionRectHeight = 65
+
+// var nodes = []
+
+var nodes = [{
+    id:"1",
+    text:"a",
+    type:"",
+    icon:"",
+    click:"",
+    children:["2", "3", "4", "5"]
+},{
+    id:"2",
+    text:"b",
+    type:"",
+    icon:"",
+    click:"",
+    children:[]
+},{
+    id:"3",
+    text:"c",
+    type:"",
+    icon:"",
+    click:"",
+    children:[]
+},{
+    id:"4",
+    text:"d",
+    type:"",
+    icon:"",
+    click:"",
+    children:["6","7"]
+},{
+    id:"5",
+    text:"e",
+    type:"",
+    icon:"",
+    click:"",
+    children:["8","9"]
+},{
+    id:"6",
+    text:"a",
+    type:"",
+    icon:"",
+    click:"",
+    children:[]
+},{
+    id:"7",
+    text:"b",
+    type:"",
+    icon:"",
+    click:"",
+    children:[]
+},{
+    id:"8",
+    text:"x",
+    type:"",
+    icon:"",
+    click:"",
+    children:[]
+},{
+    id:"9",
+    text:"y",
+    type:"",
+    icon:"",
+    click:"",
+    children:[]
+}]
 
 
 var nodeOptions = [
@@ -68,21 +127,13 @@ var nodeOptions = [
     }
 ]
 
-var branches = [{
-    parentNodeId:"1",
-    start_x: 675,
-    start_y: 102,
-    end_x:300,
-    end_y:200,
-    color:"rgb(0 0 0 / 50%)",
-}]
 
 function draw_initial_ui(){  
 
-    draw_plus(starting_x + 150, starting_y, 50)
+    draw_plus(starting_x, starting_y, 50)
 
     nodeOptions.forEach((option, index)=>{
-        draw_node_option(starting_x + 150, starting_y, option, nodeOptions.length,index)
+        draw_node_option(starting_x, starting_y, option, nodeOptions.length,index)
     })
 
 }
@@ -112,30 +163,14 @@ function draw_plus(x,y, plus_size){
 
 
 function draw_node_option(plus_x, plus_y, option, options_len, index){
-    let fontSize = 12
     // Calculate options width and start positions
     let options_width = (options_len * 120);
     let start_x = (plus_x - (options_width / 2)) + index * 120 + 12.5;
     let start_y = (plus_y + 100);
 
     // Draw the rectangle
-    draw_rect(start_x, start_y, 100, 65, blackColor, greyColor);
+    draw_rect(start_x, start_y, optionRectWidth, optionRectHeight, blackColor, greyColor, option.text, 12);
 
-    // Set font style for the context
-    ctx.font = fontSize + "px 'Poppins', sans-serif";
-
-    // Measure the text width
-    let textWidth = ctx.measureText(option.text).width;
-
-    // Calculate the horizontal position to center the text
-    let textX = start_x + (100 - textWidth) / 2  + offsetX;
-
-    // Calculate the vertical position to center the text
-    let textY = start_y + 65 / 2 + fontSize / 2  + offsetY;  
-
-    ctx.fillStyle = "black";
-    // Draw the text
-    ctx.fillText(option.text, textX, textY);
 }
 
 
@@ -157,15 +192,15 @@ function draw_filled_node(text, icon){
 }
 
 
-function draw_branch({start_x, start_y, end_x, end_y, color}){
+function draw_branch(start_x, start_y, end_x, end_y, color){
     ctx.beginPath();
     start_x = (start_x + offsetX) * scale
     start_y = (start_y + offsetY) * scale
     end_x = (end_x + offsetX) * scale
     end_y = (end_y + offsetY) * scale
     ctx.moveTo(start_x, start_y);
-    ctx.lineTo(start_x, start_y+50);
-    ctx.lineTo(end_x, start_y+50 );
+    ctx.lineTo(start_x, start_y+25);
+    ctx.lineTo(end_x, start_y+25);
     ctx.lineTo(end_x, end_y);
 
     ctx.strokeStyle = color;
@@ -175,7 +210,7 @@ function draw_branch({start_x, start_y, end_x, end_y, color}){
 }
 
 
-function draw_rect(start_x,start_y,start_width,start_height,line_color, background_color) {
+function draw_rect(start_x,start_y,start_width,start_height,line_color, background_color, text="", fontSize="") {
     // Set the radius for the rounded edges
     const cornerRadius = 20 * scale;
 
@@ -228,6 +263,25 @@ function draw_rect(start_x,start_y,start_width,start_height,line_color, backgrou
 
     ctx.stroke();
 
+    if(text){
+        ctx.font = fontSize + "px 'Poppins', sans-serif";
+
+        // Measure the text width
+        let textWidth = ctx.measureText(text).width;
+
+        // Calculate the horizontal position to center the text
+        let textX = start_x + (100 - textWidth) / 2  + offsetX;
+
+        // Calculate the vertical position to center the text
+        let textY = start_y + 65 / 2 + fontSize / 2  + offsetY;  
+
+        ctx.fillStyle = "black";
+        // Draw the text
+        ctx.fillText(text, textX, textY);
+    }
+
+    
+
 }
 
 
@@ -238,12 +292,7 @@ function draw() {
     if(!nodes || nodes.length == 0){
         draw_initial_ui()
     }else{
-        nodes.forEach((node) => {
-            draw_tree_element(node)
-        })
-        branches.forEach((branch)=>{
-            draw_branch(branch)
-        })
+        draw_tree_elements(nodes[0], starting_x, starting_y)
     }
     
 }
@@ -282,8 +331,63 @@ function draw_canvas() {
     ctx.restore();
 }
 
-function draw_tree_element(node) {
-    draw_rect(node.x, node.y, node.width, node.height, node.line_color, node.background_color)
+function draw_tree_elements(node, plus_x, plus_y) {
+    draw_rect(plus_x, plus_y, rectWidth, rectWidth, line_color, background_color, node.text, 16)
+    node.children.forEach((id, index)=>{
+        let child_node = getNodeFromId(id)
+        if(child_node){
+            let coords = get_coordinates(child_node, node, plus_x, plus_y, index)
+            draw_tree_elements(child_node, coords[0], coords[1])
+            child_node = getNodeFromId(id)
+            coords = get_coordinates(child_node, node, plus_x, plus_y, index)
+            draw_branch(plus_x+rectHeight/2+25, plus_y+rectHeight+50, coords[0]+rectWidth/2, coords[1])
+        }
+    })
+}
+
+function getNodeFromId(id){
+    for(let i = 0; i < nodes.length; i++){
+        if(nodes[i].id === id){
+            return nodes[i]
+        }
+    }
+    return null
+}
+
+function get_coordinates(node, parent, plus_x, plus_y, current_index){
+
+    let leftNeighbor = null
+    let rightNeighbor = null
+
+    if (current_index - 1 > 0) {
+        leftNeighbor = getNodeFromId(parent.children[current_index-1])
+    }
+    if (current_index + 1 < parent.children.length) {
+        rightNeighbor = getNodeFromId(parent.children[current_index+1])
+    }
+
+    let leftChildOffset = getNodeRecursiveWidth(leftNeighbor)/2
+    let rightChildOffset = getNodeRecursiveWidth(rightNeighbor)/2
+
+    let optionsLength = parent.children.length
+    let index = parent.children.indexOf(node.id)
+    let options_width = (optionsLength * (rectWidth+50));
+    let start_x = (plus_x - (options_width / 2)) + index * (rectWidth+50) + 100 + leftChildOffset;
+    let start_y = (plus_y + rectHeight+100);
+    return [start_x, start_y]
+}
+
+function getNodeRecursiveWidth(node){
+    let maxWidth = 0
+    if(node && node.children){
+        maxWidth = (rectWidth+50) * node.children.length
+        node.children.forEach((child)=>{
+            maxWidth = Math.max(getNodeRecursiveWidth(child), maxWidth)
+        })
+       
+    }
+    return maxWidth
+
 }
 
 function updateOffset(x, y) {
